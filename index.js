6489074e1e2d1e531c6ff1080c66326eadc97ee6
Alex Bierach <alex@thebrandingfarm.com>
2016-05-14 16:39:57 -0700

    Add snippet to keep Heroku free dyno awake

--- index.js
+++ index.js
@@ -73,3 +73,9 @@ controller.hears('breathe', ['direct_message', 'direct_mention', 'mention'], fun
 	});
 });
 
+// To keep Heroku's free dyno awake
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Ok, dyno is awake.');
+}).listen(process.env.PORT || 5000);
+
